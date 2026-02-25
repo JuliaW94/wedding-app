4963fc2816478d8dcc59d0530c16b6dee42dbfe5
JuliaW94 <68858542+JuliaW94@users.noreply.github.com>
2026-02-25 21:35:52 +0100

    Update src/index.tsx to remove the unnecessary React import and fix formatting

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,19 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import * as React from 'react'
 import ReactDOM from "react-dom/client";
 
-import keycloak, {initKeycloak} from "./auth/keycloak.ts";
-import {store} from "./redux";
-import {setAuthenticated} from "./redux/authSlice.ts";
-import {router} from "./router/router.tsx";
-import {RouterProvider} from "@tanstack/react-router";
-import {Provider} from "react-redux";
-import {ThemeProvider} from "@mui/material/styles";
-import {weddingTheme} from "./theme";
-import {startTokenRefresh} from "./auth/tokenRefresh.ts";
-import {CssBaseline} from "@mui/material";
+import keycloak, { initKeycloak } from "./auth/keycloak.ts";
+import { store } from "./redux";
+import { setAuthenticated } from "./redux/authSlice.ts";
+import { router } from "./router/router.tsx";
+import { RouterProvider } from "@tanstack/react-router";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "@mui/material/styles";
+import { weddingTheme } from "./theme";
+import { startTokenRefresh } from "./auth/tokenRefresh.ts";
+import { CssBaseline } from "@mui/material";
+
 await initKeycloak().then((authenticated) => {
     if (authenticated) {
         store.dispatch(
@@ -26,23 +26,20 @@ await initKeycloak().then((authenticated) => {
                     id: keycloak.tokenParsed?.sub,
                     username: keycloak.tokenParsed?.preferred_username as string,
                     email: keycloak.tokenParsed?.email as string,
-                    locale: keycloak.tokenParsed?.locale as ("de"|"en"),
-                    roles: keycloak.realmAccess?.roles ?? []
-                }
+                    locale: keycloak.tokenParsed?.locale as ("de" | "en"),
+                    roles: keycloak.realmAccess?.roles ?? [],
+                },
             })
-        )
-        startTokenRefresh()
+        );
+        startTokenRefresh();
     }
 
-
-    ReactDOM.createRoot(document.getElementById('root')!).render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <ThemeProvider theme={weddingTheme}>
-                    <CssBaseline/>
-                    <RouterProvider router={router} />
-                </ThemeProvider>
-            </Provider>
-        </React.StrictMode>
-    )
-})
+    ReactDOM.createRoot(document.getElementById("root")!).render(
+        <Provider store={store}>
+            <ThemeProvider theme={weddingTheme}>
+                <CssBaseline />
+                <RouterProvider router={router} />
+            </ThemeProvider>
+        </Provider>
+    );
+});
